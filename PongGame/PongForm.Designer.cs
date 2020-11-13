8a99527177a0bcccf2aa78b71bff980980f9b860
Tyson Gibby <tyson.gibby@outlook.com>
2020-11-12 20:08:09 -0700

    cleaned and added comments, changed most variables to self-explaining

---
+++ PongGame/PongForm.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace PongGame
+{
+    partial class PongForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PongForm));
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.playerScoreLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.computerScoreLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.humanPaddleTimer = new System.Windows.Forms.Timer(this.components);
+            this.ballTimer = new System.Windows.Forms.Timer(this.components);
+            this.computerPaddleTimer = new System.Windows.Forms.Timer(this.components);
+            this.humanPaddle = new System.Windows.Forms.PictureBox();
+            this.computerPaddle = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.humanPaddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPaddle)).BeginInit();
+            this.SuspendLayout();
+
+            // Ball
+            this.ball.BackColor = System.Drawing.Color.Black;
+            this.ball.Location = new System.Drawing.Point(227, 120);
+            this.ball.Name = "Ball";
+            this.ball.Size = new System.Drawing.Size(15, 15);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+
+            // playerScoreLabel
+            this.playerScoreLabel.AutoSize = true;
+            this.playerScoreLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.playerScoreLabel.Location = new System.Drawing.Point(163, 9);
+            this.playerScoreLabel.Name = "playerScoreLabel";
+            this.playerScoreLabel.Size = new System.Drawing.Size(20, 20);
+            this.playerScoreLabel.TabIndex = 3;
+            this.playerScoreLabel.Text = "0";
+
+            // label2
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(226, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = ":";
+
+            // computerScoreLabel
+            this.computerScoreLabel.AutoSize = true;
+            this.computerScoreLabel.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.computerScoreLabel.Location = new System.Drawing.Point(287, 9);
+            this.computerScoreLabel.Name = "computerScoreLabel";
+            this.computerScoreLabel.Size = new System.Drawing.Size(20, 20);
+            this.computerScoreLabel.TabIndex = 5;
+            this.computerScoreLabel.Text = "0";
+
+            // startButton
+            this.startButton.Location = new System.Drawing.Point(196, 209);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(80, 40);
+            this.startButton.TabIndex = 6;
+            this.startButton.Text = "Start Game";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButtonClick);
+            
+            // timer1
+            this.humanPaddleTimer.Interval = 5;
+            this.humanPaddleTimer.Tick += new System.EventHandler(this.MovePaddle);
+            
+            // timer2
+            this.ballTimer.Interval = 5;
+            this.ballTimer.Tick += new System.EventHandler(this.MoveBall);
+
+            // timer3
+            this.computerPaddleTimer.Interval = 5;
+            this.computerPaddleTimer.Tick += new System.EventHandler(this.Computer);
+
+            // Player
+            this.humanPaddle.BackColor = System.Drawing.Color.Aqua;
+            this.humanPaddle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.humanPaddle.Location = new System.Drawing.Point(0, 75);
+            this.humanPaddle.Name = "Player";
+            this.humanPaddle.Size = new System.Drawing.Size(30, 90);
+            this.humanPaddle.TabIndex = 7;
+            this.humanPaddle.TabStop = false;            
+            
+            // Computer player             
+            this.computerPaddle.BackColor = System.Drawing.Color.Red;
+            this.computerPaddle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.computerPaddle.Location = new System.Drawing.Point(454, 75);
+            this.computerPaddle.Name = "PC";
+            this.computerPaddle.Size = new System.Drawing.Size(30, 90);
+            this.computerPaddle.TabIndex = 8;
+            this.computerPaddle.TabStop = false;
+            
+            // PongForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 261);
+            this.Controls.Add(this.computerPaddle);
+            this.Controls.Add(this.humanPaddle);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.computerScoreLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.playerScoreLabel);
+            this.Controls.Add(this.ball);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PongForm";
+            this.Text = "Pong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyPressed);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyReleased);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.humanPaddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPaddle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Label playerScoreLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label computerScoreLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Timer humanPaddleTimer;
+        private System.Windows.Forms.Timer ballTimer;
+        private System.Windows.Forms.Timer computerPaddleTimer;
+        private System.Windows.Forms.PictureBox humanPaddle;
+        private System.Windows.Forms.PictureBox computerPaddle;
+    }
+}
+
---
+++ PongGame/PongForm.cs
@@ -0,0 +1,290 @@
+﻿// based on src: https://www.codeproject.com/Articles/5250284/Small-WinForm-Pong-Game-Csharp
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PongGame
+{
+    public partial class PongForm : Form
+    {
+        public PongForm()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+        const int limitPad = 170;
+        const int limitBall = 245;
+        const int x = 277;  // x ball starting posistion
+        const int y = 120;  // y ball starting position
+
+        int computerScore = 0;  // tracks computer's score
+        int playerScore = 0;  // tracks player's score
+
+        int yBallPosition;  // tracks ball's vertical position (up and down)
+        int xBallPosition; // tracks ball's horizontal position (left and right)
+
+        bool moveUp = false;  //  tracks if player wants to move paddle up
+        bool moveDown = false;  // tracks if player wants to move paddle down
+        bool gameInProgress = false;  // tracks if game is in progress
+
+        readonly Random randomPosition = new Random(Guid.NewGuid().GetHashCode());
+
+        // toggles paddle movement to true when up or down keys are press
+        private void KeyPressed(object sender, KeyEventArgs e)
+        {
+            if (gameInProgress)
+            {
+                if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+                {
+                    moveUp = true;
+                }
+                else if (e.KeyCode == Keys.Down || e.KeyCode == Keys.S)
+                {
+                    moveDown = true;
+                }
+                humanPaddleTimer.Start(); 
+            }
+        }
+
+        // toggles paddle movement to false when up or down keys are press
+        private void KeyReleased(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up || e.KeyCode == Keys.W)
+            {
+                moveUp = false;
+            }
+            else if (e.KeyCode == Keys.Up || e.KeyCode == Keys.S)
+            {
+                moveDown = false;
+            }
+            humanPaddleTimer.Stop();
+        }
+        
+        // moves paddle to new position
+        private void MovePaddle(object sender, EventArgs e)
+        {
+            if (moveUp && humanPaddle.Location.Y > 0)
+            {
+                humanPaddle.Top -= 3; 
+            }
+            else if (moveDown && humanPaddle.Location.Y < limitPad)
+            {
+                humanPaddle.Top += 3;
+            }
+        }
+
+        // moves ball to new position
+        private void MoveBall(object sender, EventArgs e)
+        {
+            if (ball.Bounds.IntersectsWith(humanPaddle.Bounds))
+            {
+                Collision(humanPaddle);
+            }
+            else if (ball.Bounds.IntersectsWith(computerPaddle.Bounds))
+            {
+                Collision(computerPaddle);
+            }
+            HitBorder();
+            BallLeftField();
+            BallMoves();
+        }
+
+        // tracks collision of ball with player paddles
+        private void Collision(PictureBox Paddle) 
+        {
+            switch (true)
+            {
+                case true when Upper(Paddle):
+                    yBallPosition = Negative(4, 6);
+                    xBallPosition = AdjustCoordinates(5, 6);
+                    break;
+                case true when High(Paddle):
+                    yBallPosition = Negative(2, 3);
+                    xBallPosition = AdjustCoordinates(6, 7);
+                    break;
+                case true when Middle(Paddle):
+                    yBallPosition = 0;
+                    xBallPosition = AdjustCoordinates(5, 5);
+                    break;
+                case true when Low(Paddle):
+                    yBallPosition = randomPosition.Next(2, 3);
+                    xBallPosition = AdjustCoordinates(6, 7);
+                    break;
+                case true when Bot(Paddle):
+                    yBallPosition = randomPosition.Next(4, 6);
+                    xBallPosition = AdjustCoordinates(5, 6);
+                    break;                   
+            }
+            Edge(); 
+        }
+
+        // Adjusts ball x axis movement after a collision
+        private int AdjustCoordinates(int i,int n)
+        {
+            int res = 0; 
+
+            if (ball.Location.X < this.Width / 2)
+            {
+                res = randomPosition.Next(i, n);
+            }
+            else if (ball.Location.X > this.Width / 2)
+            {
+                res = Negative(i, n);
+            }
+            return res;
+        }
+
+
+        private int Negative(int i,int n)
+        {
+            int myval = randomPosition.Next(i, n) * -1;
+            return myval; 
+        }
+
+        private bool Upper(PictureBox Pad)
+        {
+            return ball.Location.Y >= Pad.Top - ball.Height && ball.Location.Y <= Pad.Top + ball.Height;
+        }
+
+        private bool High(PictureBox Pad)
+        {
+            return ball.Location.Y > Pad.Top + ball.Height && ball.Location.Y <= Pad.Top + 2 * ball.Height;
+        }
+
+        private bool Middle(PictureBox Pad)
+        {
+            return ball.Location.Y > Pad.Top + 2 * ball.Height && ball.Location.Y <= Pad.Top + 3 * ball.Height;
+        }
+
+        private bool Low(PictureBox Pad)
+        {
+            return ball.Location.Y > Pad.Top + 3 * ball.Height && ball.Location.Y <= Pad.Top + 4 * ball.Height;
+        }
+
+        private bool Bot(PictureBox Pad)
+        {
+            return ball.Location.Y > Pad.Top + 4 * ball.Height && ball.Location.Y <= Pad.Bottom + ball.Height;
+        }
+
+        private void HitBorder()
+        {
+            if (ball.Location.Y <= 0 || ball.Location.Y >= limitBall)
+            {
+                yBallPosition *= -1; 
+            }
+        }
+
+
+        private void BallLeftField()
+        {
+            // end game when a score is 10
+            if (playerScore == 10 || computerScore == 10)
+            {
+                EndGame();
+            }
+
+            // increase computer score when ball crosses player goal
+            if (ball.Location.X < 0 - humanPaddle.Width && ball.Location.X < this.Width / 2)
+            {
+                NewPoint(5);
+                ComputerPlayerScored(); 
+            }
+            // increase player score when ball crosses computer goal
+            else if (ball.Location.X > computerPaddle.Location.X + computerPaddle.Width && ball.Location.X > this.Width / 2)
+            {
+                NewPoint(-5);
+                HumanPlayerScored(); 
+            }
+        }
+
+        private void Edge()
+        {
+            if (ball.Location.X < this.Width / 2)
+            {
+                if (ball.Location.X < 0 + ball.Height / 3)
+                {
+                    xBallPosition *= -1; 
+                }
+            }
+            else if (ball.Location.X > this.Width / 2)
+            {
+                if (ball.Location.X > computerPaddle.Location.X + (ball.Width /3))
+                {
+                    xBallPosition *= -1;
+                }
+            }
+        }
+        private void NewPoint(int _horizontalSpeed)
+        { 
+            ball.Location = new Point(x, y);
+            yBallPosition = 0;
+            xBallPosition = _horizontalSpeed;
+        }
+        private void StartValues()
+        {
+            yBallPosition = 0;
+            xBallPosition = -5; 
+        }
+        private void BallMoves()
+        {
+            ball.Top += yBallPosition;
+            ball.Left += xBallPosition; 
+        }
+        private void Computer(object sender, EventArgs e)
+        {
+            if (computerPaddle.Location.Y <= 0)
+            {
+                computerPaddle.Location = new Point(computerPaddle.Location.X, 0); 
+            }
+            else if (computerPaddle.Location.Y >= limitPad)
+            {
+                computerPaddle.Location = new Point(computerPaddle.Location.X, limitPad);
+            }
+            if (ball.Location.Y < computerPaddle.Top + (computerPaddle.Height / 2))
+            {
+                computerPaddle.Top -= 3;
+            }
+            else if (ball.Location.Y > computerPaddle.Top + (computerPaddle.Height / 2))
+            {
+                computerPaddle.Top += 3;
+            }
+        }
+
+
+        private void HumanPlayerScored()
+        {
+            playerScore++;
+            playerScoreLabel.Text = playerScore.ToString();
+        }
+
+        // increments ComputerPlayer's score and updates the scoreboard
+        private void ComputerPlayerScored()
+        {
+            computerScore++;
+            computerScoreLabel.Text = computerScore.ToString(); 
+        }
+        private void startButtonClick(object sender, EventArgs e)
+        {
+            StartValues(); 
+            gameInProgress = true;
+            startButton.Visible = false;
+            humanPaddleTimer.Start();
+            ballTimer.Start();
+            computerPaddleTimer.Start(); 
+        }
+        private void EndGame()
+        {
+            humanPaddle.Location = new Point(0, 75);
+            computerPaddle.Location = new Point(454, 75);
+            gameInProgress = false;
+            playerScore = 0;
+            computerScore = 0;
+            playerScoreLabel.Text = playerScore.ToString();
+            computerScoreLabel.Text = computerScore.ToString();
+            humanPaddleTimer.Stop();
+            ballTimer.Stop();
+            computerPaddleTimer.Stop();
+            startButton.Visible = true; 
+        }
+    }
+}
